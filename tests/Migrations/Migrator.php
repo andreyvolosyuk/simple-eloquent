a3c2a93056cf46fa232b2552682f512ed104f52d
Andrey Volosyuk <valasiuk.andrei@gmail.com>
2018-03-22 21:35:46 +0300

    5.5.4 belongs_to_many's query builder might not have isSimple method

--- tests/Migrations/Migrator.php
+++ tests/Migrations/Migrator.php
@@ -16,7 +16,8 @@ class Migrator
         ArticleCategory::class,
         Comment::class,
         Like::class,
-        Likable::class
+        Likable::class,
+        User::class
     ];
 
     /**
---
+++ tests/Models/User.php
@@ -0,0 +1,13 @@
+<?php
+
+class User extends \Illuminate\Database\Eloquent\Model
+{
+    protected $table = 'users';
+
+    public $timestamps = [];
+
+    public function articles()
+    {
+        return $this->belongsToMany(Article::class, 'article_users');
+    }
+}
